d2b50adee833559d6174827c6c2027b60c2cb032
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-07-14 13:26:21 -0300

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:app_agendamento/core/flavor/flavor_config.dart';
 void main() {
   bootstrap(
     FlavorConfig(
-      flavor: AppFlavor.prod,
-      baseUrl: 'www.google.com.br',
-      appId: '',
-      restKey: '',
+      flavor: AppFlavor.dev,
+      baseUrl: 'https://parseapi.back4app.com/functions',
+      appId: 'aTF0s4pymeWyw1m6R7YWSl6npiQkN8XmEtBvjVsa',
+      restKey: 'T8mBHK100ovBD2Pu8RabV6ox1SPzV5hY2LzPX7dH',
     ),
   );
 }
