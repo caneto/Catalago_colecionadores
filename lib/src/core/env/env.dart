2cb615378ada615e91402233825b2f423b4e9492
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2025-12-28 18:55:31 -0300

    Atualização

--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -1,21 +1,21 @@
-// lib/env/env.dart
-import 'package:envied/envied.dart';
+// // lib/env/env.dart
+// import 'package:envied/envied.dart';
 
-part 'env.g.dart';
+// part 'env.g.dart';
 
-@Envied(path: '.env')
-abstract class Env {
+// @Envied(path: '.env')
+// abstract class Env {
 
-    @EnviedField(varName: 'baseUrl')
-    static String baseUrl = _Env.baseUrl;
+//     @EnviedField(varName: 'baseUrl')
+//     static String baseUrl = _Env.baseUrl;
 
-    @EnviedField(varName: 'appId',obfuscate: true)
-    static String appId = _Env.appId;
+//     @EnviedField(varName: 'appId',obfuscate: true)
+//     static String appId = _Env.appId;
     
-    @EnviedField(varName: 'restKey',obfuscate: true)
-    static String restKey = _Env.restKey;
+//     @EnviedField(varName: 'restKey',obfuscate: true)
+//     static String restKey = _Env.restKey;
 
-    @EnviedField(varName: 'clientKey',obfuscate: true)
-    static String clientKey = _Env.clientKey;
+//     @EnviedField(varName: 'clientKey',obfuscate: true)
+//     static String clientKey = _Env.clientKey;
     
-}
+// }
